2a82bba8bd33b83514d5b4b5f5fa703e9e1fd12b
Nithin koundinya <nithinkoundinya@gmail.com>
2022-01-12 18:32:05 +0000

    production ready

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module TestApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults "6.0"
     config.autoloader = :classic
-
+    config.eager_load = true
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
